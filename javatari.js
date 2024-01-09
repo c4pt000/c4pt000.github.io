021623f349d50b7f81119bc3401ad34b296ad8a1
https://bitnet-io.org <46433702+c4pt000@users.noreply.github.com>
2024-01-09 03:02:26 -0700

    Update javatari.js

--- javatari.js
+++ javatari.js
@@ -9,7 +9,7 @@ Javatari = {
     PRESETS:                        "",                         // Configuration Presets to apply. See Presets Configuration
 
     // Full or relative URL of Media files to load
-    CARTRIDGE_URL:                  "Pepsi.bin",
+    CARTRIDGE_URL:                  "https://c4pt000.github.io/Pepsi.bin",
     AUTODETECT_URL:                 "",
     STATE_URL:                      "",
 
